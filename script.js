1e9c0a40502f08d74a9c09e8d52b7d6147deda14
rolandvasarhelyi <roland.vasarhelyi@bentley.com>
2024-04-20 16:18:16 +0100

    Fixing the vulnerability of visible api

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 "use strict";
 
+const apiKey = config.MY_KEY;
 const button = document.querySelector(".btn");
 
 function getInput() {
   const input = document.querySelector("input").value;
 
   fetch(
-    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=iivZwJbuLRSmT8FkFQFsnprbAKdGSAyu=${input}`
+    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}=${input}`
   )
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
       const locationId = data[0].Key;
       return fetch(
-        `http://dataservice.accuweather.com/currentconditions/v1/${locationId}?apikey=iivZwJbuLRSmT8FkFQFsnprbAKdGSAyu`
+        `http://dataservice.accuweather.com/currentconditions/v1/${locationId}?apikey=${apiKey}`
       )
         .then((res) => res.json())
         .then((data) => {
