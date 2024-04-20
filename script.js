2802badc32ee7be0902811d46d04935d40f7a0ed
rolandvasarhelyi <roland.vasarhelyi@bentley.com>
2024-04-20 16:32:23 +0100

    Changed the API from AccuWeather to OpenWeather

--- script.js
+++ script.js
@@ -2,26 +2,28 @@
 
 const apiKey = config.MY_KEY;
 const button = document.querySelector(".btn");
+const paragraph = document.querySelector("p");
 
 function getInput() {
   const input = document.querySelector("input").value;
 
   fetch(
-    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}=${input}`
+    `http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=${config.MY_KEY}`
   )
     .then((res) => res.json())
     .then((data) => {
+      const lat = data[0].lat;
+      const lon = data[0].lon;
       console.log(data);
-      const locationId = data[0].Key;
+      console.log(lat, lon);
+
       return fetch(
-        `http://dataservice.accuweather.com/currentconditions/v1/${locationId}?apikey=${apiKey}`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${config.MY_KEY}`
       )
         .then((res) => res.json())
         .then((data) => {
-          console.log(data);
-          const weatherText = data[0].WeatherText;
-          document.querySelector(".city-text").innerText = input;
-          document.querySelector("p").innerText = weatherText;
+          const description = data.weather[0].description;
+          paragraph.innerText = description;
         });
     });
 }
