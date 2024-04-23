0848e3435df849ae9d80a6519e3b6078bd309c1f
rolandvasarhelyi <roli.vasarhelyi@gmail.com>
2024-04-23 18:32:29 +0100

    Added the icons for different weather conditions and created error handling

--- script.js
+++ script.js
@@ -3,31 +3,50 @@
 const apiKey = config.MY_KEY;
 const button = document.querySelector(".btn");
 const cityText = document.querySelector(".city-name");
-const paragraph = document.querySelector("p");
+const cardTemp = document.querySelector(".card-bottom__temp");
+const cardDesc = document.querySelector(".card-bottom__desc");
+const welcome = document.querySelector(".welcome-message");
+const card = document.querySelector(".card");
+const cardImage = document.querySelector(".card-mid__img");
 
 function getInput() {
   const inputCity = document.querySelector("input").value;
-  cityText.innerText = inputCity;
-  fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${inputCity}&appid=${config.MY_KEY}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      const lat = data[0].lat;
-      const lon = data[0].lon;
-      console.log(data);
-      console.log(lat, lon);
+  if (inputCity == "") {
+    alert("You must enter a city name");
+    return false;
+  } else {
+    cityText.innerText = inputCity.charAt(0).toUpperCase() + inputCity.slice(1);
+    welcome.classList.add("hidden");
+    card.classList.remove("hidden");
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${inputCity}&appid=${config.MY_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+        console.log(data);
+        console.log(lat, lon);
 
-      return fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${config.MY_KEY}`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          console.log();
-          const description = data.weather[0].description;
-          paragraph.innerText = description;
-        });
-    });
+        return fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${config.MY_KEY}`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            const description = data.weather[0].description;
+            const temperature = data.main.temp;
+            cardDesc.innerText = description;
+            console.log(description);
+            cardImage.src = `images/${description}.png`;
+            cardTemp.innerText = `${Math.ceil(temperature - 273.15)}°C`;
+          });
+      })
+      .catch((err) => {
+        alert("Please enter a valid city name");
+      });
+    document.querySelector("input").value = "";
+  }
 }
 
 button.addEventListener("click", getInput);
